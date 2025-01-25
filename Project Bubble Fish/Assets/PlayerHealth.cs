6fe1d4cc6b0edd84116d75087f51f9cd3209cbec
Sawyer <sawyerholmes19@gmail.com>
2025-01-25 19:41:58 -0400

    added player iframes and health logic

---
+++ Project Bubble Fish/Assets/PlayerHealth.cs
@@ -0,0 +1,70 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth {get; private set;}
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth < 0)
+        {
+            anim.SetTrigger("Hurt");
+            StartCoroutine(Invulnerability());
+        }
+        else 
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovementScript>().enabled = false;
+                dead = true;
+            }    
+        }
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration/ (numberOfFlashes * 2));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+}
