132594a72e4fa772f931da87042ef732f9638b64
Sawyer <sawyerholmes19@gmail.com>
2025-01-25 20:48:08 -0400

    started healthbar

---
+++ Project Bubble Fish/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+    
+
+    private void Start()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+
+    private void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
